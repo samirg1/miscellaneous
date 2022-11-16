ed32df68893a01f74e337984aa35b16d57208c6a
Samir Gupta <srgupta@bigpond.com>
2022-11-16 20:37:25 +1100

    get chat id from user

--- wordle_chat_summary.py
+++ wordle_chat_summary.py
@@ -3,14 +3,15 @@
 # Author: Samir Gupta
 
 from sqlite3 import (
-    connect as sql_db_connect,
     Connection as SQLConnection,
     OperationalError as SQLOperationalError,
+    connect as sql_db_connect
 )
-from pandas import read_sql_query
+from typing import cast
+from pandas import read_sql_query, DataFrame
 
 
-def get_messages(conn: SQLConnection):
+def get_messages(connection: SQLConnection):
 
     # get the 10 entries of the message table using pandas
     messages = read_sql_query(
@@ -27,10 +28,10 @@ def get_messages(conn: SQLConnection):
                 select ROWID from chat where LOWER(display_name) = "wordle crew"
             )
         """,
-        conn,
+        connection,
     )
 
-    chat = read_sql_query(
+    _ = read_sql_query(
         """
         select 
             *
@@ -39,33 +40,53 @@ def get_messages(conn: SQLConnection):
         where
             chat_id = 2
         """,
-        conn,
+        connection,
     )
 
     print(messages)
 
 
 def get_db_connection() -> SQLConnection:
-    conn: SQLConnection
+    connection: SQLConnection
     while True:
         user = input("enter mac username : ")
         try:
-            conn = sql_db_connect(f"/Users/{user}/Library/Messages/chat.db")
+            connection = sql_db_connect(f"/Users/{user}/Library/Messages/chat.db")
             break
         except SQLOperationalError:
             print("invalid username - try again")
-    return conn
+    return connection
+
+
+def get_chat_id(connection: SQLConnection) -> int:
+    chat_name: str = ''
+    chat_id_query: DataFrame = DataFrame()
+    while True:
+        chat_name = input("enter wordle chat name : ").lower()
+        chat_id_query = read_sql_query(
+            f"""
+            SELECT
+                ROWID
+            FROM
+                chat
+            WHERE
+                LOWER(display_name) = "{chat_name}"
+            """,
+            connection,
+        )
+        try: 
+            if chat_id_query.empty or not chat_name:
+                raise ValueError
+            break
+        except:
+            print('invalid chat name - try again')
+
+    return int(cast(str, chat_id_query["ROWID"][0]))
 
 
 def main():
-    conn = get_db_connection()
-
-    # connect to the database
-    cur = conn.cursor()
-    # get the names of the tables in the database
-    cur.execute(" select name from sqlite_master where type = 'table' ")
-
-    get_messages(conn)
+    connection = get_db_connection()
+    print(get_chat_id(connection))
 
 
 if __name__ == "__main__":
