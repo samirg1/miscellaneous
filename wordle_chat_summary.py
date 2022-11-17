0f5c5feca8df5dad273a97a8049b8c4b46f81661
Samir Gupta <srgupta@bigpond.com>
2022-11-17 13:53:02 +1100

    clean ups and small fixes

--- wordle_chat_summary.py
+++ wordle_chat_summary.py
@@ -2,6 +2,7 @@
 # Date: 16-11-2022
 # Author: Samir Gupta
 
+from math import inf, log10
 from os import listdir
 from sqlite3 import Connection, OperationalError, connect
 from typing import Callable, Iterable, Iterator, Sequence, TypeVar, cast
@@ -16,6 +17,7 @@ class User:
         self.name = name
         self.number = number
         self._games: dict[int, int] = {}
+        self.fails = 0
 
     def __repr__(self) -> str:
         return f"({self.name}, {self.number}, {self._games})"
@@ -28,13 +30,17 @@ class User:
         return len(self._games)
 
     @property
+    def attempted(self) -> int:
+        return self.completed + self.fails
+
+    @property
     def average_guesses(self) -> float:
         if self.completed == 0:
             return 0
         return sum(self._games.values()) / self.completed
 
 
-def find(sequence: Sequence[T], callable: Callable[[T], bool]) -> T | None:
+def find(sequence: Sequence[T], /, *, key: Callable[[T], bool]) -> T | None:
     """
     Find the first element in a sequence that satisfies a condition.
     - Input:
@@ -45,7 +51,7 @@ def find(sequence: Sequence[T], callable: Callable[[T], bool]) -> T | None:
     - Aux space complexity: O(1).
     """
     for element in sequence:
-        if callable(element):
+        if key(element):
             return element
 
 
@@ -179,7 +185,8 @@ def get_users_from_numbers(numbers: Iterable[str]) -> list[User]:
                 found_users.append(User(name, number))
                 break
 
-    self_name = input("Enter user's display name: ")
+    print("\nENTER USER NAME")
+    self_name = input("enter user's display name: ")
     found_users.append(User(self_name, ""))
     return found_users
 
@@ -218,54 +225,81 @@ def get_messages(connection: Connection, chat_id: int) -> Iterator[tuple[str, st
     return (cast(tuple[str, str], messages.iloc[i, :]) for i in range(len(messages)))  # return iterator of info access
 
 
-def anaylse_messages(members: list[User], messages: Iterator[tuple[str, str]]) -> int:
-    wordle_number: int
-    guesses: int
+def anaylse_messages(members: list[User], messages: Iterator[tuple[str, str]]) -> int | float:
+    """
+    Analyse messages to add wordle games to chat members.
+    - Input:
+        - members (list[User]): The chat members.
+        - messages (Iterator[tuple[str, str]]): The messages in form (text, number).
+    - Returns (int): The amount of days the messages were from or 0 if no messages were found.
+    """
     max_wordle_number: int = 0
-    min_wordle_number: int = 10_000
+    min_wordle_number: int | float = inf
     for text, phone in messages:
-        match text.split(" "):
-            case ["Wordle", no, score, *_]:
-                print(text)
+        fail_detected = False
+        match text.split(" "):  # match texts to 'Wordle <num> <guesses>/6
+            case ["Wordle", number, score, *_]:
                 try:
-                    wordle_number = int(no)
-                    guesses = int(score[0])
-
+                    wordle_number = int(number)
                     max_wordle_number = max(max_wordle_number, wordle_number)
                     min_wordle_number = min(min_wordle_number, wordle_number)
                 except ValueError:
                     continue
-            case _:
+
+                try:
+                    guesses = int(score[0])
+                except ValueError:
+                    if score[0] == "X":  # if user fails it will be X/6
+                        fail_detected = True
+                        guesses = 0
+                    else:
+                        continue
+            case _:  # continue if text doesnt match
                 continue
 
-        member = find(members, lambda member: member.number == phone) if phone is not None else members[-1]
+        member = find(members, key=lambda member: member.number == phone) if phone is not None else members[-1]  # find the member, if phone is None member is user
         if member is not None:
-            member.add_wordle(wordle_number, guesses)
+            if fail_detected:
+                member.fails += 1
+            else:
+                member.add_wordle(wordle_number, guesses)  # add wordle
 
-    return max_wordle_number - min_wordle_number + 1
+    return max(max_wordle_number - min_wordle_number + 1, 0)  # return total amount of days or 0 if no messages
 
 
 def print_summary(members: list[User], messages: Iterator[tuple[str, str]]):
-    total_days = anaylse_messages(members, messages)
-    max_name_length = len(max(members, key=lambda user: len(user.name)).name)
+    """
+    Print the summary of the Wordle group chat.
+    - Input:
+        - members (list[User]): The members in the chat.
+        - messages (Iterator[tuple[str, str]]): The messages in the chat.
+    """
+    total_days = anaylse_messages(members, messages)  # analyse messages
+    if total_days == 0:
+        return print("\nno wordle messages found")
 
-    members.sort(key=lambda user: user.completed, reverse=True)
-    print(f"\nCOMPLETION SUMMARY ({total_days} days)")
-    for i, member in enumerate(members, start=1):
-        print(f"{i}. {member.name:{max_name_length}} - {member.completed}")
+    max_name_length = len(max(members, key=lambda user: len(user.name)).name)  # get the length of the longest name
+    max_completed = max(members, key=lambda user: user.completed).completed  # get the maximum amount of completions
+    completed_width = int(log10(max_completed)) + 1  # get width for completion
 
-    members.sort(key=lambda user: user.average_guesses)
-    print("\nAVERAGE GUESSES")
+    members.sort(key=lambda user: user.completed, reverse=True)  # sort by completions
+
+    print(f"\nCOMPLETIONS ({total_days} days)")  # print completion summary
     for i, member in enumerate(members, start=1):
-        print(f"{i}. {member.name:{max_name_length}} - {member.average_guesses:.2f}")
+        print(f"{i}. {member.name:.<{max_name_length}}..{member.completed:.>{completed_width}d}/{member.attempted}")
+
+    members.sort(key=lambda user: user.average_guesses)  # sort by average guesses
+    print("\nAVERAGE GUESSES")  # print average guess summary
+    for i, member in enumerate(members, start=1):
+        print(f"{i}. {member.name:.<{max_name_length}}..{member.average_guesses:.2f}")
 
 
 def main():
-    connection = get_db_connection()
-    chat_id = get_chat_id(connection)
-    members = get_chat_members(connection, chat_id)
-    messages = get_messages(connection, chat_id)
-    print_summary(members, messages)
+    connection = get_db_connection()  # get main db connection
+    chat_id = get_chat_id(connection)  # get chat id of wordle chat
+    members = get_chat_members(connection, chat_id)  # get members of the chat
+    messages = get_messages(connection, chat_id)  # get the messages of the chat
+    print_summary(members, messages)  # print the summary of the chat
 
 
 if __name__ == "__main__":
